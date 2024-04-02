03284a532adc43900c816989c1d4fd12ead395e9
KhayalSadigov <aynurisrafilova9@gmail.com>
2024-04-02 00:34:29 -0700

    new-file added

---
+++ app.js
@@ -0,0 +1,71 @@
+//  let a=4;
+//  let b=4;
+//  if (a-b>0) {
+//     console.log ("a greater than b");
+//  }
+//  else if (a=b){
+//     console.log("a equal b")
+//  }
+//  else {
+//     console.log ("b greater than a");
+//  }
+// let a=5;
+// let b=5;
+// let c=5;
+// if (a+b>c && b+c>a && a+c>b) {
+//     console.log("true");
+// }
+ 
+// else {
+//     console.log("false");
+// }
+// let a=3;
+// let b=4;
+// let c=5;
+// if (a*a +b**b==c**c || b*b+ c**c == a**a || a*a+ c**c==b**b ) {
+//    console.log(true )
+// }
+// else {
+//     console.log(false)
+// } 
+ let balans= 0.10
+ let gedishaqqi=0.40
+ let emeliyyat=3;
+ let mebleg=2
+//  if (c>=b) {
+//    console.log("welcome" ,c-b); 
+// }
+ 
+//  else {
+//     console.log("kifayet qeder mebleg yoxdur", b-c );
+    
+// } 
+// if (emeliyyat=1 ){
+//     console.log("men metroya kecid etmek isteyirem");
+// }
+// else console.log("men balansimi artirmaq isteyirem:", c+mebleg );
+//  switch (emeliyyat) {
+//     case 1: 
+//      if (balans>gedishaqqi) {
+//         console.log("welcome" ,balans-gedishaqqi); 
+//      }
+      
+//       else {
+//          console.log("kifayet qeder mebleg yoxdur", gedishaqqi-balans );
+//     break;
+// }
+//     case 2: 
+//     console.log("artirilan balans:", balans+mebleg)
+//     case 3 : console.log("mem metroya kecid etmek isteyirem")
+//     }
+let input= 3431
+let a=Math.floor (3431/1000)
+let b=Math.floor ((3431%1000)/100)
+let c=Math.floor ((3431%100)/10)
+let d=input%10
+let cem=a+b+c+d ;
+console.log (cem)
+let hasil=a*b*c*d;
+console.log(hasil)
+let ferq=a-b-c-d;
+console.log(ferq)
